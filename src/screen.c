1b707e59363a328c839ffef54e2e4b173f83bd4b
Louis Richards <llouis.richards@gmail.com>
2023-11-24 13:49:09 +0100

    Log to file
    Logging now offers the function "enable_logging_to_file()" that creates a log.txt
    and then populates the file with the logs.

--- src/screen.c
+++ src/screen.c
@@ -3,30 +3,40 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "../include/utils.h"
+#include "utils.h"
+#include <string.h>
 
 int main(void)
 {
-	char *args = malloc(sizeof(char)); /* command line arguments */
+	enable_logging_to_file();
+	char *args = NULL; /* command line arguments */
+	size_t args_length = 0;			   /* size of args buffer */
+	ssize_t nread;					   /* number of characters read */
 	int should_run = 1;				   /* flag to determine when to exit program */
 
 	while (should_run)
 	{
-		fprintf(stderr, "osh>");
-		fflush(stderr);
-
-		size_t args_length = 0;
-		if (getline(&args, &args_length, stdin) == -1)
+		fprintf(stdout, "osh>");
+		
+		if ((nread = getline(&args, &args_length, stdin)) == -1)
 		{
-			log_error("Error reading input...");
+			log_error("Error reading input...\n");
 			exit(EXIT_FAILURE);
 		}
 
 		log_debug("args: %s", args);
 
+		/* TODO: Temporary exit condition */
+		if (strcmp(args, "exit\n") == 0)
+		{
+			close_log_file();
+			should_run = 0;
+		}
+
 		/* TODO: Send the command to the shell */
 	}
-
+	
+	log_debug("Freeing args...\n");
 	free(args);
 
 	return 0;
